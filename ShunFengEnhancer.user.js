1e5f131be32dcb715fe698d1c38b9fbd4d8101a8
magoer <maoyantsing@gmail.com>
2020-09-09 21:46:57 +0800

    update V0.3

--- ShunFengEnhancer.user.js
+++ ShunFengEnhancer.user.js
@@ -2,7 +2,7 @@
 // @name         ShunFengEnhancer
 // @icon         https://www.sf-express.com/.gallery/favicon.ico
 // @homepage     https://github.com/maoger/ShunFengEnhancer
-// @version      0.2
+// @version      0.3
 // @description  Show all routes of ShunFeng.
 // @author       Maoger
 // @match        http*://*.sf-express.com/*
@@ -16,14 +16,14 @@ btn_show = document.createElement("div");
 tag_body = document.querySelector("body");
 tag_body.appendChild(btn_show);
 btn_show.innerHTML = "展开";
-btn_show.style = "position:fixed;bottom:15px;right:15px;width:60px;height:60px;background:black;opacity:0.75;color:white;text-align:center;line-height:60px;cursor:pointer;";
+btn_show.style = "position:fixed;bottom:50%;right:15px;width:60px;height:60px;background:black;opacity:0.75;color:white;text-align:center;line-height:60px;cursor:pointer;";
 
 function show_routes(){
     document.getElementsByClassName("routes-wrapper")[0].style = "max-height:100%";
 }
 
 btn_show.onclick = function(){
-    this.style.display = "none";
+    // this.style.display = "none";
     show_routes();
 };
 
