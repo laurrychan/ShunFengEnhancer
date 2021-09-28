dc8db1d3c242dade7266107a8966a18e08e55185
maoger <maoyantsing@gmail.com>
2021-09-28 23:32:39 +0800

    update V0.5.5
    删除：展开按钮;优化：支持一次性下载10个快递查询结果

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # ShunFengEnhancer - 顺丰助手
 
-![ShunFengEnhancer](./media/ShunFengEnhancer.gif)
+![](./img/screenshot_01.png)
 
-## 功能
+## 功能简介
 
-- 展开：全屏展开显示快递轨迹信息
-- 截图：全屏截图
+- 截图并下载：点击<kbd>截图</kbd>按钮，即可自动展开所有快递轨迹详情、截图并下载
+- 自动重命名<sup style = "color:red;font-style:italic;">New!</sup>：按照“**快递单号**.png”格式标准重命名截图
+- 批量下载<sup style = "color:red;font-style:italic;">New!</sup>：支持一次性下载**10**个快递查询结果
+
+## 输出示例
+
+![](./img/screenshot_02.png)
 
 ## 更新日志
 
--- ShunFengEnhancer.user.js
+++ ShunFengEnhancer.user.js
@@ -2,59 +2,47 @@
 // @name         ShunFengEnhancer
 // @icon         https://www.sf-express.com/.gallery/favicon.ico
 // @homepage     https://github.com/maoger/ShunFengEnhancer
-// @version      0.5.4
+// @version      0.5.5
 // @description  顺丰助手
 // @author       Maoger
 // @match        http*://*.sf-express.com/*
 // @require      https://cdn.bootcdn.net/ajax/libs/html2canvas/0.5.0-beta4/html2canvas.min.js
-// @grant        GM_openInTab
 // @run-at       document-idle
 // @updateURL    https://openuserjs.org/meta/maoger/ShunFengEnhancer.meta.js
 // @copyright    2020-2021, maoger (https://openuserjs.org/users/maoger)
 // @license      MIT
 // ==/UserScript==
 
-var currentHeight = document.body.clientHeight;
-var showHeight = currentHeight / 2 - 35;
-var screenshotHeight = showHeight + 70;
-var btn_show = document.createElement("div");
 var tag_body = document.querySelector("body");
-tag_body.appendChild(btn_show);
-btn_show.innerHTML = "展开";
-btn_show.style = "position:fixed;z-index:999;top:" + showHeight.toString() + "px;right:15px;width:60px;height:60px;background:black;opacity:0.75;color:white;text-align:center;line-height:60px;cursor:pointer;";
 var btn_webshot = document.createElement("div");
 tag_body.appendChild(btn_webshot);
 btn_webshot.innerHTML = "截图";
-btn_webshot.style = "position:fixed;z-index:999;top:" + screenshotHeight.toString() + "px;right:15px;width:60px;height:60px;background:black;opacity:0.75;color:white;text-align:center;line-height:60px;cursor:pointer;";
-function show_routes(){
-    var eles = document.getElementsByClassName("routes-wrapper");
-    for(var i=0;i<eles.length; i++){
-        eles[i].style = "max-height:100%";
-    }
-}
-btn_show.onclick = function(){
-    this.style.display = "none";
-    show_routes();
-    setTimeout(function(){
-        btn_show.style.display = "block";
-    },100)
-};
-function webshot(){
-    window.scrollTo(0,0);
-    html2canvas(document.body, {
-        background : "#fff",
-        onrendered : function(canvas) {
-            var imgUrl = canvas.toDataURL();
-            GM_openInTab(imgUrl);
-        }
-    });
-}
+var currentHeight = document.body.clientHeight;
+var height_screenshot = currentHeight / 2;
+btn_webshot.style = "position:fixed;z-index:999;top:" + height_screenshot.toString() + "px;right:15px;width:60px;height:60px;background:black;opacity:0.75;color:white;text-align:center;line-height:60px;cursor:pointer;";
 btn_webshot.onclick = function(){
     this.style.display = "none";
-    btn_show.style.display = "none";
     webshot();
     setTimeout(function(){
         btn_webshot.style.display = "block";
-        btn_show.style.display = "block";
     },2000)
+};
+function webshot(){
+    var eles = document.getElementsByClassName("delivery");
+    for(var j=0; j<eles.length; j++){
+        eles[j].getElementsByClassName("routes-wrapper")[0].style = "max-height:100%";
+        download_img(eles[j]);
+    }
+};
+function download_img(ele){
+    var billNum = ele.querySelector('span.number').innerText;
+    html2canvas(ele, {
+        background : "#fff",
+        onrendered : function(canvas) {
+            var img = document.createElement('a');
+            img.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+            img.download = billNum + '.png';
+            img.click();
+        }
+    });
 };
